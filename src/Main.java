71f588b4e091ceef683e5ab1f6d286c6bc9f518d
ChaseJ <145057536+Lehua18@users.noreply.github.com>
2025-06-17 18:28:56 -0500

    Started sorting txt files

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,56 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Objects;
 import java.util.Scanner;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.FileNotFoundException;
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Scanner scan = new Scanner(uploadTxtFile());
-            ArrayList<String[]> lineInfo = new ArrayList<>();
+            ArrayList<Objects[]> lineInfo = new ArrayList<>();
+            //order: year, month, day, yearasFraction, sunspot #, standard deviation; # observations, prov indicator
             do{
+                String line = scan.nextLine();
+                int length = line.length();
+                String type = "";
+                if(length == 37){
+                 //daily and monthly = 37
+                    int year = Integer.parseInt(line.substring(0,4));
+                    int month = Integer.parseInt(line.substring(5,7));
+                    int day = Integer.parseInt(line.substring(8,10));
+                    double yrAsDec = Double.parseDouble(line.substring(11,19));
+                    int sunspotNum = Integer.parseInt(line.substring(21,24));
+                    double standDev = Double.parseDouble(line.substring(25,30));
+                    int numObs = Integer.parseInt(line.substring(31,35));
+                    String provInd = line.substring(36);
+                    type = "daily";
+                } else if (length == 28) {
+                    double yrAsDec = Double.parseDouble(line.substring(0,7));
+                    int sunspotNum = Integer.parseInt(line.substring(8,13));
+                    double standDev = Double.parseDouble(line.substring(14,19));
+                    String provInd = line.substring(27);
+                    type = "yearly";
+                } else if (length == 66) {
+                    int year = Integer.parseInt(line.substring(0,4));
+                    int month = Integer.parseInt(line.substring(5,7));
+                    int day = Integer.parseInt(line.substring(8,10));
+                    double yrAsDec = Double.parseDouble(line.substring(11,19));
+                    int sunspotNum = Integer.parseInt(line.substring(21,24));
+                    int nSunspotNum = Integer.parseInt(line.substring(25,28));
+                    int sSunspotNum = Integer.parseInt(line.substring(29,32));
+                    double standDev = Double.parseDouble(line.substring(34,39));
+                    double nStandDev = Double.parseDouble(line.substring(40,45));
+                    double sStandDev = Double.parseDouble(line.substring(45,51));
+                    int numObs = Integer.parseInt(line.substring(31,35));
+                    String provInd = line.substring(36);
+                    type = "hemDaily";
+                }else {
+
+                }
+
 
             }while(scan.hasNext());
         }catch(FileNotFoundException e){

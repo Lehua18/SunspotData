de14abd9cfb5a0725d42622b8c6b8cedfc9ac7d5
ChaseJ <145057536+Lehua18@users.noreply.github.com>
2025-06-26 11:15:35 -0500

    Got a poly this time!

--- src/main/java/Grapher.java
+++ src/main/java/Grapher.java
@@ -7,9 +7,6 @@ import org.knowm.xchart.style.*;
 import org.knowm.xchart.style.colors.*;
 import org.knowm.xchart.style.lines.SeriesLines;
 
-//Math Stuff
-import org.apache.commons.math4.legacy.fitting.PolynomialCurveFitter;
-import org.apache.commons.math4.legacy.fitting.WeightedObservedPoint;
 
 //General Imports
 import java.awt.*;
@@ -18,9 +15,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Grapher {
-    private double total;
-    private boolean parity = false;
     private ArrayList<Double> coeff;
+
     //3D Grapher
     public Grapher(double[] x, double[] y, double[] z) {
         RinearnGraph3D graph = new RinearnGraph3D();
@@ -36,6 +32,8 @@ public class Grapher {
         double endDate = Double.parseDouble(scan.nextLine());
         System.out.println("Please enter a ending year for the approximation");
         double endApproxDate = Double.parseDouble(scan.nextLine());
+
+        //find approx index of endpoints
         int startIndex = -1;
         int endIndex = -1;
         int endApproxIndex = -1;
@@ -93,10 +91,11 @@ public class Grapher {
 //        PolynomialCurveFitter fitter = PolynomialCurveFitter.create(15);
 ////        fitter = fitter.getO
 //        double[] coefficients = fitter.fit(points);
-        
+
+        //Creates a new array list with approximated values
         ArrayList<Double> xApprox = new ArrayList<>();
         ArrayList<Double> yApprox = new ArrayList<>();
-        int degree = 5;
+        int degree = 2;
         double center = xTruncData[xTruncData.length/2];
         for(double d = startDate; d<endDate; d+= 0.08){
             //Nullify any rounding errors
@@ -104,12 +103,10 @@ public class Grapher {
             d= Math.round(d);
             d/=100.0;
             xApprox.add(d);
-            System.out.println("d: "+d);
-            yApprox.add(taylorApproxOuter(degree, xTruncData, yTruncData, d, center));
+            //System.out.println("d: "+d);
             
         }
         ArrayList<Double> xAfterApprox = new ArrayList<>();
-        ArrayList<Double> yAfterApprox = new ArrayList<>();
         for(double f = endDate; f<endApproxDate; f+= 0.08){
             //Nullify rounding error
             f*=100;
@@ -117,33 +114,29 @@ public class Grapher {
             f/=100.0;
 
             xAfterApprox.add(f);
-            yAfterApprox.add(taylorApproxOuter(degree, xTruncData, yTruncData, f, center));
         }
         
 
         //Create 2D graph
-        XYChart chart = new XYChartBuilder().width(800).height(600).title("Sunspot Number Over Time").xAxisTitle("Time (years)").yAxisTitle("Sunspot Number").build();
+        XYChart chart = new XYChartBuilder().width(800).height(600).title("Sunspot Number Over Time").xAxisTitle("Year").yAxisTitle("Sunspot Number").build();
+
         // Customize Chart
         chart.getStyler().setPlotBackgroundColor(Color.WHITE);
         chart.getStyler().setPlotGridLinesVisible(true);
         chart.getStyler().setPlotGridLinesColor(Color.GRAY);
-
         chart.getStyler().setChartBackgroundColor(Color.WHITE);
         chart.getStyler().setLegendBackgroundColor(Color.WHITE);
         chart.getStyler().setLegendBorderColor(Color.BLACK);
         chart.getStyler().setChartFontColor(Color.BLACK);
         chart.getStyler().setChartTitleBoxVisible(false);
         chart.getStyler().setChartTitleBoxBorderColor(null);
-
         chart.getStyler().setAxisTickPadding(0);
         chart.getStyler().setXAxisMin(startDate);
         chart.getStyler().setXAxisMax(endApproxDate);
         chart.getStyler().setYAxisMax(300.0);
         chart.getStyler().setYAxisMin(0.0);
         chart.getStyler().setAxisTickMarkLength(15);
-
         chart.getStyler().setPlotMargin(20);
-
         chart.getStyler().setChartTitleFont(new Font(Font.SERIF, Font.BOLD, 24));
         chart.getStyler().setLegendFont(new Font(Font.SERIF, Font.PLAIN, 18));
         chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSE);
@@ -153,11 +146,14 @@ public class Grapher {
         chart.getStyler().setDatePattern("MM-dd");
         chart.getStyler().setDecimalPattern("#0");
         chart.getStyler().setLocale(Locale.ENGLISH);
-        XYSeries dataSeries = chart.addSeries("Sunspot Data", xTruncData, yTruncData);
-        XYSeries approxSeries = chart.addSeries("Approx Sunspot Data", xApprox, yApprox);
-        XYSeries futureSeries = chart.addSeries("After Sunspot Data", xAfterData,yAfterData);
-        XYSeries futureApproxSeries = chart.addSeries("Predicted Sunspot Data", xAfterApprox, yAfterApprox);
 
+        //Add data to graph
+        XYSeries dataSeries = chart.addSeries("Sunspot Data", xTruncData, yTruncData);
+        XYSeries approxSeries = chart.addSeries("Approx Sunspot Data", xApprox, taylorApproxOuter(degree,xTruncData,yTruncData,center,startDate,endDate));
+        XYSeries futureSeries = chart.addSeries("After Sunspot Data", xAfterData,yAfterData);
+        XYSeries futureApproxSeries = chart.addSeries("Predicted Sunspot Data", xAfterApprox, taylorApproxOuter(degree,xTruncData,yTruncData,center,startDate,endDate));
+
+        //Style individual series
         dataSeries.setLineColor(XChartSeriesColors.BLUE);
         dataSeries.setLineStyle(SeriesLines.NONE);
         futureSeries.setLineColor(XChartSeriesColors.GREEN);
@@ -169,9 +165,11 @@ public class Grapher {
         futureApproxSeries.setLineStyle(SeriesLines.SOLID);
         futureApproxSeries.setSmooth(true);
 
+        //display chart
         new SwingWrapper<XYChart>(chart).displayChart();
     }
-    
+
+    //Get coefficients of least squares approx method
     public double squaresApprox(double x, double[] coeff){
         double output = 0;
         for(int b = coeff.length-1; b>=0; b--){
@@ -180,23 +178,23 @@ public class Grapher {
         return output;
     }
 
-    public double taylorApproxOuter(int iterations, double[] xData, double[] yData, double num, double center){
-        total = 0;
-//        System.out.println("Center: "+center);
+    public ArrayList<Double> taylorApproxOuter(int iterations, double[] xData, double[] yData, double center, double startDate, double endDate) {
+        double total = 0;
+       System.out.println("Center: "+center);
         int centerIndex = centerIndex(xData, center);
-        double[] newXdata = new double[iterations+1];
-        double[] newYData = new double[iterations+1];
+        double[] newXdata = new double[iterations + 1];
+        double[] newYData = new double[iterations + 1];
         int count = 0;
-        if (iterations%2 == 0){
-            int diff = iterations/2;
-            for(int i = centerIndex-diff; i <= centerIndex+diff; i++){
+        if (iterations % 2 == 0) {
+            int diff = iterations / 2;
+            for (int i = centerIndex - diff; i <= centerIndex + diff; i++) {
                 newXdata[count] = xData[i];
                 newYData[count] = yData[i];
                 count++;
             }
-        }else{
-            int diff = iterations/2;
-            for(int i = centerIndex-diff; i <= centerIndex+diff+1; i++){
+        } else {
+            int diff = iterations / 2;
+            for (int i = centerIndex - diff; i <= centerIndex + diff + 1; i++) {
                 newXdata[count] = xData[i];
                 newYData[count] = yData[i];
                 count++;
@@ -204,66 +202,91 @@ public class Grapher {
         }
 
          centerIndex = centerIndex(newXdata, center);
-        double last = taylorApproxInner(iterations,newXdata,newYData,num,center);
-        System.out.println("Total for "+num+": "+(total  + newYData[centerIndex]));
-        return total  + newYData[centerIndex];
+        coeff = new ArrayList<Double>();
+        coeff.add(newYData[centerIndex]);
+        double last = taylorApproxCoeff(iterations,newXdata,newYData,center);
+        System.out.print("FINAL COEFFICIENTS: [");
+        for(double co : coeff){
+            System.out.print(co+", ");
+        }
+        System.out.println("]");
+        ArrayList<Double> points = new ArrayList<>();
+        for(double num = startDate; num<endDate; num+=0.08) {
+            //Nullify rounding error
+            num*=100;
+            num= Math.round(num);
+            num/=100.0;
+
+            total = taylorApproxSum(num,center);
+            System.out.println("Total for "+num+": "+ total);
+            points.add(total);
+        }
+        System.out.println("Total points: "+ points.size());
+
+        return points;
 
     }
 
-    public double taylorApproxInner(int iterations, double[] xData, double[] yData, double num, double center){
+    public double taylorApproxCoeff(int iterations, double[] xData, double[] yData, double center){
+
+
         System.out.print(iterations+" xdata: [");
         for(double x : xData){
             System.out.print(x+", ");
         }
         System.out.println("]");
+        System.out.print("\t"+iterations+" ydata: [");
+        for(double x : yData){
+            System.out.print(x+", ");
+        }
+        System.out.println("]");
         if(iterations == 1){
-            total += (yData[1] - yData[0])/(xData[1] - xData[0])*(num-center);
+            System.out.println("check");
+            coeff.add((yData[1] - yData[0])/(xData[1] - xData[0]));
             return  (yData[1] - yData[0])/(xData[1] - xData[0]);
         }else{
-            parity = !parity;
+            System.out.println("notcheck");
             int centIndex = centerIndex(xData,center);
-            double next = ((taylorApproxInner(iterations - 1, arrayShortener(xData,centIndex, parity, true),
-                            arrayShortener(yData, centIndex, parity, true), num, center) -
-                    taylorApproxInner(iterations - 1, arrayShortener(xData,centIndex, parity, false),
-                            arrayShortener(yData, centIndex, parity, false), num, center))/
-                    (((sum(arrayShortener(xData,centIndex,parity,true)))/(iterations+1)) -
-                    ((sum(arrayShortener(xData,centIndex, parity,false)))/(iterations+1))))
+            double next = ((taylorApproxCoeff(iterations - 1, arrayShortener(xData,centIndex, true),
+                            arrayShortener(yData, centIndex, true), center) -
+                    taylorApproxCoeff(iterations - 1, arrayShortener(xData,centIndex, false),
+                            arrayShortener(yData, centIndex, false), center))/
+                    (((sum(arrayShortener(xData,centIndex,true)))/(iterations)) -
+                    ((sum(arrayShortener(xData,centIndex,false)))/(iterations))))
             ;
-            total+= (next*Math.pow(num - center,iterations))/(factorial(iterations));
-
+            coeff.add(next);
             return next;
 
         }
-
-
     }
 
-    public double[] arrayShortener(double[] oldArr, int centerIndex, boolean parity, boolean first){
-        double[] newArr = new double[oldArr.length -1];
-        int count = 0;
-        if(newArr.length == 2){
-            if(first){
-                newArr[0] = oldArr[0];
-                newArr[1] = oldArr[1];
-            }else{
-                newArr[0] = oldArr[1];
-                newArr[1] = oldArr[2];
-            }
-        }else {
-            if (first) {
-               for(int i = 0; i<newArr.length; i++) {
-                newArr[i] = oldArr[i];
-               }
 
-            } else {
-                for (int i = 0; i< newArr.length; i++){
-                    newArr[i] = oldArr[i+1];
-                }
+    public double taylorApproxSum(double num, double center){
+        double total = 0;
+        double difference = num-center;
+        for(int i = 0; i<coeff.size(); i++){
+            total+= ((coeff.get(i)*Math.pow(difference,i))/factorial(i));
+        }
+        return total;
+    }
+
+    //Shortens array by 1 index depending on placement
+    public double[] arrayShortener(double[] oldArr, int centerIndex, boolean first){
+        double[] newArr = new double[oldArr.length -1];
+        if (first) {
+            for(int i = 0; i<newArr.length; i++) {
+                newArr[i] = oldArr[i];
+            }
+
+        } else {
+            for (int i = 0; i< newArr.length; i++){
+                newArr[i] = oldArr[i+1];
             }
         }
         return newArr;
     }
 
+    //Gets the index of the 'center' of an array
     public int centerIndex(double[] arr, double center){
         int centerIndex = -1;
         for(int j = 0; j< arr.length; j++){
@@ -273,6 +296,8 @@ public class Grapher {
         }
         return centerIndex;
     }
+
+    //Adds all elements in an array
     public double sum(double[] arr){
         double total = 0;
         for (double v : arr) {
@@ -281,10 +306,13 @@ public class Grapher {
         return total;
     }
 
+    //Takes the factorial of a number
     public int factorial(int num){
         int total = 1;
-        for(int i = num; i> 0; i--){
-            total *= i;
+        if(num != 0 && num != 1) {
+            for (int i = num; i > 0; i--) {
+                total *= i;
+            }
         }
         return total;
     }
